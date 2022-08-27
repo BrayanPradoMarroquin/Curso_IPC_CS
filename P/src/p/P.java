cf292e24a118decd0bbf30fee631acb6f62a78b4
Brayan-PM <78266710+BrayanPradoMarroquin@users.noreply.github.com>
2022-08-27 15:20:59 -0600

    Primer Ejemplo

---
+++ P/src/p/P.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p;
+
+/**
+ *
+ * @author Usuario
+ */
+public class P {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {System.out.println("Hola Mundo");
+        // TODO code application logic here
+        
+        System.out.println("Mucho Gusto");
+    }
+    
+}
